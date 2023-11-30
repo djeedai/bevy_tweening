cd7068c90de04fa7b0e63f07f65c08470bcad728
ebola <dev@axiomatic>
2023-11-29 21:08:22 -0500

    Initial pass updating to bevy main

--- src/lens.rs
+++ src/lens.rs
@@ -95,12 +95,12 @@ impl Lens<Text> for TextColorLens {
     fn lerp(&mut self, target: &mut Text, ratio: f32) {
         // Note: Add<f32> for Color affects alpha, but not Mul<f32>. So use Vec4 for
         // consistency.
-        let start: Vec4 = self.start.into();
-        let end: Vec4 = self.end.into();
+        let start: Vec4 = self.start.rgba_to_vec4();
+        let end: Vec4 = self.end.rgba_to_vec4();
         let value = start.lerp(end, ratio);
 
         if let Some(section) = target.sections.get_mut(self.section) {
-            section.style.color = value.into();
+            section.style.color = Color::rgba_from_array(value);
         }
     }
 }
@@ -335,10 +335,10 @@ pub struct UiBackgroundColorLens {
 #[cfg(feature = "bevy_ui")]
 impl Lens<BackgroundColor> for UiBackgroundColorLens {
     fn lerp(&mut self, target: &mut BackgroundColor, ratio: f32) {
-        let start: Vec4 = self.start.into();
-        let end: Vec4 = self.end.into();
+        let start: Vec4 = self.start.rgba_to_vec4();
+        let end: Vec4 = self.end.rgba_to_vec4();
         let value = start.lerp(end, ratio);
-        target.0 = value.into();
+        target.0 = Color::rgba_from_array(value);
     }
 }
 
@@ -360,10 +360,10 @@ impl Lens<ColorMaterial> for ColorMaterialColorLens {
     fn lerp(&mut self, target: &mut ColorMaterial, ratio: f32) {
         // Note: Add<f32> for Color affects alpha, but not Mul<f32>. So use Vec4 for
         // consistency.
-        let start: Vec4 = self.start.into();
-        let end: Vec4 = self.end.into();
+        let start: Vec4 = self.start.rgba_to_vec4();
+        let end: Vec4 = self.end.rgba_to_vec4();
         let value = start.lerp(end, ratio);
-        target.color = value.into();
+        target.color = Color::rgba_from_array(value);
     }
 }
 
@@ -385,10 +385,10 @@ impl Lens<Sprite> for SpriteColorLens {
     fn lerp(&mut self, target: &mut Sprite, ratio: f32) {
         // Note: Add<f32> for Color affects alpha, but not Mul<f32>. So use Vec4 for
         // consistency.
-        let start: Vec4 = self.start.into();
-        let end: Vec4 = self.end.into();
+        let start: Vec4 = self.start.rgba_to_vec4();
+        let end: Vec4 = self.end.rgba_to_vec4();
         let value = start.lerp(end, ratio);
-        target.color = value.into();
+        target.color = Color::rgba_from_array(value);
     }
 }
 
