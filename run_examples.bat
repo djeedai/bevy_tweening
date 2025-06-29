df01c5a9dc91f141ca1038135e615940df3315c5
Jerome Humbert <djeedai@gmail.com>
2025-06-29 16:05:29 +0100

    Fix example build script

--- run_examples.bat
+++ run_examples.bat
@@ -1,15 +1,15 @@
 @echo on
 echo Run all examples
 REM Default
-cargo r --example menu --no-default-features --features="bevy_ui bevy_text bevy/bevy_winit"
-cargo r --example transform_translation --no-default-features --features="bevy_sprite bevy/bevy_winit"
-cargo r --example transform_rotation --no-default-features --features="bevy_sprite bevy/bevy_winit"
-cargo r --example sequence --no-default-features --features="bevy_sprite bevy_text bevy/bevy_winit"
+cargo r --example menu --no-default-features --features="bevy_ui bevy_text bevy/bevy_winit bevy/bevy_picking"
+cargo r --example transform_translation --no-default-features --features="bevy_sprite bevy/bevy_winit bevy/bevy_picking"
+cargo r --example transform_rotation --no-default-features --features="bevy_sprite bevy/bevy_winit bevy/bevy_picking"
+cargo r --example sequence --no-default-features --features="bevy_sprite bevy_text bevy/bevy_winit bevy/bevy_picking"
 REM bevy_sprite
-cargo r --example sprite_color --no-default-features --features="bevy_sprite bevy/bevy_winit"
+cargo r --example sprite_color --no-default-features --features="bevy_sprite bevy/bevy_winit bevy/bevy_picking"
 REM bevy_ui
-cargo r --example ui_position --no-default-features --features="bevy_sprite bevy_ui bevy/bevy_winit"
+cargo r --example ui_position --no-default-features --features="bevy_sprite bevy_ui bevy/bevy_winit bevy/bevy_picking"
 REM bevy_text
-cargo r --example text_color --no-default-features --features="bevy_text bevy_ui bevy/bevy_winit"
+cargo r --example text_color --no-default-features --features="bevy_text bevy_ui bevy/bevy_winit bevy/bevy_picking"
 REM bevy_sprite + bevy_asset
-cargo r --example colormaterial_color --no-default-features --features="bevy_asset bevy_sprite bevy/bevy_winit"
+cargo r --example colormaterial_color --no-default-features --features="bevy_asset bevy_sprite bevy/bevy_winit bevy/bevy_picking"
