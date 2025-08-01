0de4630d9bf1c6e70c428b40836b9a46912a2553
Jerome Humbert <djeedai@gmail.com>
2025-08-01 21:54:05 +0100

    Fix example launch command

--- run_examples.bat
+++ run_examples.bat
@@ -5,6 +5,7 @@ cargo r --example menu --no-default-features --features="bevy_ui bevy_text bevy/
 cargo r --example transform_translation --no-default-features --features="bevy_sprite bevy/bevy_winit bevy/bevy_picking"
 cargo r --example transform_rotation --no-default-features --features="bevy_sprite bevy/bevy_winit bevy/bevy_picking"
 cargo r --example sequence --no-default-features --features="bevy_sprite bevy_text bevy/bevy_winit bevy/bevy_picking"
+cargo r --example ambient_light --no-default-features --features="bevy_ui bevy_text bevy/bevy_winit bevy/bevy_picking bevy/bevy_pbr bevy/hdr bevy/tonemapping_luts"
 REM bevy_sprite
 cargo r --example sprite_color --no-default-features --features="bevy_sprite bevy/bevy_winit bevy/bevy_picking"
 cargo r --example colormaterial_color --no-default-features --features="bevy_sprite bevy/bevy_winit bevy/bevy_picking"
